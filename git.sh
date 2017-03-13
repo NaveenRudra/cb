201fb6668088bee773012f6bc514f5069b4fb30e
leonrom <leonrom@rambler.ru>
2017-03-13 19:37:41 +0000

    change names of values

---
+++ git.sh
@@ -0,0 +1,12 @@
+# git init
+git add . 
+git commit -m "first commit"
+#git remote add origin   https://github.com/leonrom/cb.git
+git pull
+git push -u origin master
+
+git log --pretty=oneline
+git tag -a v1.0.1 -m 'version 1.0.2' b2e6bb49
+git push origin v1.0.1
+
+git rm --cached *.txt
