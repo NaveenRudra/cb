9956afce8a19f017a64d9470222159dfe9e3db53
leonrom <leonrom@rambler.ru>
2017-03-15 18:19:08 +0000

    add test for 100 executions

--- cb100/j1.php
+++ cb100/j1.php
@@ -2,17 +2,12 @@
 /*
  * Cople j.js+j.php of working example of JSONP code. Leon Rom, 2017
 */
-    $f = fopen( './logJ.txt', "w" );
-    fwrite( $f, "started" . "\n");
 
     $prms = $_REQUEST['prms'];
     $func = $_REQUEST['func'];
-    fwrite( $f, "prms=$prms, func=$func" . "\n" ); 
 
-    $t = "$prms = " . time() ;
-    fwrite( $f, "finish : t=$t" . "\n" );                
+    $t = "$prms = " . time() ;                
         
     echo($func.'({"result":"' . $t . '",' . '"typ":"addUser"})');  
     
-    fclose( $f);
 ?>  
--- cb100/x1.php
+++ cb100/x1.php
@@ -2,13 +2,10 @@
 /*
  * Cople x.js+x.php of working example of CORS code. Leon Rom, 2017
 */
-    $f = fopen( './logX.txt', "w" );
-    fwrite( $f, "started X: HTTP_ORIGIN=" . $_SERVER['HTTP_ORIGIN'] ."\n" );
 
     $http_origin = $_SERVER['HTTP_ORIGIN'];
     $urlp = parse_url($http_origin);
     $host = $urlp[host];
-    fwrite( $f, "host==$host" . "\n" );
 
     $allowed_hosts = [              // from which  hosts  acces wil be allowed
         'romleon.rf.gd',
@@ -19,7 +16,6 @@
         'crossbrowser-leonrom.c9users.io',  
     ];
 
-    fwrite( $f, "set headers"  . "\n");
     header("Access-Control-Allow-Origin: {$http_origin}");
     header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Max-Age: 86400'); // cache for 1 day
@@ -28,18 +24,15 @@
     header("Access-Control-Allow-Headers: 
             {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
 
-    if ((in_array($host, $allowed_hosts)) || ($host == ""))  // Decide if allow access
+    if (in_array($host, $allowed_hosts))   // Decide if allow access
     {         
         $nam = "parms";
         $prm = "?";
         if (isset($_REQUEST[$nam])) 
-            $prm = $_REQUEST[$nam];    
-        fwrite( $f, "finish: prm=$prm" . "\n" );                
+            $prm = $_REQUEST[$nam];                  
         
         echo ("CORS <b>$prm</b> !");
-    }else{
-        fwrite( $f, "your domain is NOT allowed" . "\n" );                
+    }else{              
         echo ("Ваш домен не допущен");
     }
-    fclose( $f);
 ?>
